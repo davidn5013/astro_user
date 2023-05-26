4f88e20c46543cea05473677b0ed2a7af39e94c2
David Nilsson <davidn5013@yahoo.se>
2023-05-26 15:14:22 +0200

    Moved shortcuts

--- mappings.lua
+++ mappings.lua
@@ -17,28 +17,24 @@ return {
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
+    
+  },
+  n = {
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["ö"] = { "{", desc = "Prev paragraf" },
+    ["ä"] = { "}", desc = "Next paragraf" },
+    ["å"] = { "$", desc = "End of line" },
+    ["gj"] = {"<cmd>GoAlt<cr>"},
+    ["gi"] = {"<cmd>GoImport<cr>"},
+    ["cp"] = {'<esc>:let @* = expand("%:p:h")<cr>:echo "Path copied to clipboard"<cr>'},
+    ["<leader>d"] = { "<cmd>e $userprofile\\appdata\\local\\nvim\\lua\\user\\mappings.lua<cr>", desc = "Open Usermappings"},
     ["<leader>8"] = { "<cmd>e $clipb\\nottoself.txt<cr>", desc = "Open Not To Self"},
     ["<leader>9"] = { "<cmd>e $clipb\\clipboard.txt<cr>", desc = "Open Clipboard file"},
+    ["bö"] = { "<cmd>bp<cr>", desc = "Prev buffer" },
+    ["bä"] = { "<cmd>bn<cr>", desc = "Next buffer" },
   },
   t = {    -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
-    ["bö"] = { "<cmd>bp<cr>", desc = "Prev buffer" },
-    ["bä"] = { "<cmd>bn<cr>", desc = "Next buffer" },
-    
-    ["ö"] = { "{", desc = "Prev paragraf" },
-    ["ä"] = { "}", desc = "Next paragraf" },
-
-    ["å"] = { "$", desc = "End of line" },
-
-    ["<leader>d"] = { "<cmd>e $userprofile\\appdata\\local\\nvim\\lua\\user\\mappings.lua<cr>", 
-                      desc = "Open Usermappings"},
-    ["gj"] = {"<cmd>GoAlt<cr>"},
-    ["gi"] = {"<cmd>GoImport<cr>"},
-    
-    ["cp"] = {'<esc>:let @* = expand("%:p:h")<cr>:echo "Path copied to clipboard"<cr>'},
     -- tables with the `name` key will be registered with which-key if it's installed
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
